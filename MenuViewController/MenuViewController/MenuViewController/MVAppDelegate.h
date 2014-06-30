e151560b86f7dd03bd74da364914583cd5bf4487
skswkd5 <skswkd5@solutus.co.kr>
2014-06-30 22:26:22 +0900

    3번째 숙제: MenuViewController 생

---
+++ MenuViewController/MenuViewController/MenuViewController/MVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MVAppDelegate.h
+//  MenuViewController
+//
+//  Created by Jeesun Kim on 2014. 6. 30..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
