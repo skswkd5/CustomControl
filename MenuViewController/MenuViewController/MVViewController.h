7e65e14dfee53aafb81110d51275b90d7828fac4
skswkd5 <skswkd5@solutus.co.kr>
2014-07-21 23:54:09 +0900

    3번째 숙제 메뉴뷰 다시 올리기

---
+++ MenuViewController/MenuViewController/MVViewController.h
@@ -0,0 +1,13 @@
+//
+//  MVViewController.h
+//  MenuViewController
+//
+//  Created by Jeesun Kim on 2014. 6. 30..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVViewController : UIViewController <UIGestureRecognizerDelegate, UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ MenuViewController/MenuViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MenuViewController
+//
+//  Created by Jeesun Kim on 2014. 6. 30..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MVAppDelegate class]));
+    }
+}
